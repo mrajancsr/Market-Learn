d29bfd9f44b27268c532bc378011fcc1c1d160b5
RajanS <mrajancsr@gmail.com>
2020-10-08 21:22:08 -0400

    added some linear collections

--- marketlearn/algorithms/linear_collections.py
+++ marketlearn/algorithms/linear_collections.py
@@ -3,6 +3,7 @@
 from typing import Any, Union, List, Optional
 import collections
 
+
 class _Empty(Exception):
     """prints Exception error if array is empty
 
@@ -13,27 +14,28 @@ class _Empty(Exception):
     """
     pass
 
+
 class Stack:
     """Implementation of stacks using lists
-    
+
        O(1) time for all operations
        O(n) time for max operation
-       Assumes LIFO; 
+       Assumes LIFO;
        items on the right is the top, left is bottom
     """
-    def __init__(self, data: Optional[list]=None):
+    def __init__(self, data: Optional[list] = None):
         self._data = [] if data is None else data
-    
+
     def empty(self):
         """returns true if stack is empty"""
         return self._data == []
-    
+
     def pop(self):
         """removes item from right(top) of stack"""
         if self.empty():
             raise _Empty("Stack is empty")
         return self._data.pop()
-    
+
     def push(self, item: Any) -> None:
         """adds item to right(top) of stack
 
@@ -41,7 +43,7 @@ class Stack:
         :type item: Any
         """
         self._data.append(item)
-    
+
     def peek(self) -> Any:
         """returns right(top) item of stack
 
@@ -49,7 +51,7 @@ class Stack:
         :rtype: Any
         """
         return self._data[-1]
-    
+
     def size(self) -> int:
         """returns number of items in stack
 
@@ -57,7 +59,7 @@ class Stack:
         :rtype: int
         """
         return len(self._data)
-    
+
     def max(self) -> Union[int, float]:
         """returns the maximum item in stack
 
@@ -71,18 +73,18 @@ class Stack:
 
 class MaxStack:
     """Implementation of stacks using lists
-    
+
        O(1) time for all operations
-       Assumes LIFO; 
+       Assumes LIFO;
        items on the right is the top, left is bottom
     """
     # to keep track of maximum value after each push
     _Items = collections.namedtuple("_Items", ('item', 'max'))
 
-    def __init__(self, item: Optional[list]=None):
+    def __init__(self, item: Optional[list] = None):
         self._data = []
         self._create_stack(item)
-        
+
     def _create_stack(self, items: list) -> list:
         """Creates a stack from list of items
 
@@ -103,7 +105,7 @@ class MaxStack:
         :rtype: bool
         """
         return self._data == []
-    
+
     def max(self) -> Union[float, int]:
         """returns maximum value in a stack
 
@@ -116,7 +118,7 @@ class MaxStack:
         if self.empty():
             raise _Empty("Stack is empty")
         return self._data[-1].max
-    
+
     def pop(self) -> Any:
         """removes item from the right(top) of stack
 
@@ -127,16 +129,16 @@ class MaxStack:
         if self.empty():
             raise _Empty("Stack is empty")
         return self._data[-1].item
-    
+
     def push(self, item):
         """adds item to right(top) of stack
 
         :param item: item to be pushed
         :type item: Any
         """
-        self._data.append(\
-            self._Items(item, item
-            if self.empty() else max(item, self.max())))
+        self._data.append(self._Items(item, item
+                          if self.empty() else max(item, self.max())))
+
 
 class Queue:
     """Implementation of Queue using a list
@@ -148,21 +150,22 @@ class Queue:
     def __init__(self):
         """Default constructor, needs no parameters"""
         self._data = []
-    
+
     def empty(self):
         return self._data == []
-    
+
     def enqueue(self, item):
         self._data.append(item)
-    
+
     def dequeue(self):
         return self._data.pop(0)
-    
+
     def size(self):
         return len(self._data)
-    
+
     def max(self):
         return max(self._data)
+
 
 class CircularQueue:
     """Implementation of a Queue using a circular Array
@@ -178,22 +181,22 @@ class CircularQueue:
         self._data = [None] * self._DEFAULT_CAPACITY
         self._size = 0
         self._front = 0
-    
+
     def __len__(self):
         return self._size
-    
+
     def size(self):
         return len(self)
-    
+
     def empty(self):
         return self.size() == 0
-    
+
     def front(self):
         """Returns, but doesn't remove first element"""
         if self.empty():
             raise IndexError("Queue is Empty")
         return self._data[self._front]
-    
+
     def dequeue(self):
         if self.empty():
             raise IndexError("Queue is Empty")
@@ -212,7 +215,7 @@ class CircularQueue:
         avail = (self._front + self.size()) % len(self._data)
         self._data[avail] = item
         self._size += 1
-    
+
     def _resize(self, cap):
         old = self._data
         self._data = [None] * cap
@@ -227,10 +230,10 @@ class StackQueue:
     """Implementation of Queue using Stacks"""
     def __init__(self):
         self._enq, self._deq = Stack(), Stack()
-    
+
     def enqueue(self, item):
         self._enq.push(item)
-    
+
     def dequeue(self):
         if not self._deq:
             while self._enq:
@@ -238,15 +241,16 @@ class StackQueue:
         if not self._deq:
             raise IndexError("Empty Queue")
         return self._deq.pop()
-    
+
     def front(self):
         return self._enq.peek()
-    
+
+
 class MaxQueue:
     """Implementation of Queue withh max API using Deque"""
     def __init__(self):
         self._data = collections.deque()
         self._candidates_for_max = collections.deque()
-    
+
     def enqueue(self, item):
         pass
