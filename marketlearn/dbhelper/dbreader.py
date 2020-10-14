fae7de18e219ee6f4ecfb4c7b2311fe85f330072
RajanS <mrajancsr@gmail.com>
2020-10-14 19:03:11 -0400

    added time profiler for whenever data is pushed or retrieved from database

--- marketlearn/dbhelper/dbreader.py
+++ marketlearn/dbhelper/dbreader.py
@@ -4,11 +4,16 @@ Author: Rajan Subramanian
 Created May 10 2020
 Todo - add a copy from and time profiler 
 """
-import psycopg2, os, time, pandas as pd
-from functools import wraps
+
+import os
+import pandas as pd
+import psycopg2
 from configparser import ConfigParser
+from functools import wraps
+from marketlearn.toolz import execution_time
 from psycopg2.extras import execute_values, DictCursor, execute_batch
 from typing import Iterator, List, Tuple, Dict, Any
+
 
 class DbReader:
     """Establishes a sql connection with the PostGres Database
@@ -68,8 +73,9 @@ class DbReader:
 
     def _create_records(self, dictrow: List) -> Tuple:
         """converts data obtained from db into tuple of dictionaries"""
-        return tuple({k:v for k,v in record.items()} for record in dictrow)
+        return tuple({k: v for k, v in record.items()} for record in dictrow)
 
+    @execution_time
     def fetch(self, query: str, section: str = 'dev'):
         """Returns the data associated with table
         Args:
@@ -94,6 +100,7 @@ class DbReader:
         else:
             return rows
 
+    @execution_time
     def fetchdf(self, query: str, section: str = 'dev'):
         """Returns a pandas dataframe of the db query"""
         return pd.DataFrame(self.fetch(query, section), columns=self.col_names)
@@ -104,29 +111,72 @@ class DbReader:
         """
         yield from iter(datadf.to_dict(orient='rows'))
 
-    def push(self, data: Iterator[Dict[str, Any]], table_name: str, columns: List[str], section: str = 'dev') -> None:
+    @execution_time
+    def push(self,
+             data: Iterator[Dict[str, Any]],
+             table_name: str,
+             columns: List[str],
+             section: str = 'dev',
+             ) -> None:
+        """Pushes data given as an Iterator to PostGresDB
+
+        :param data: data to be pushed
+        :type data: Iterator[Dict[str, Any]]
+        :param table_name: the table to insert data into
+        :type table_name: str
+        :param columns: the columns where data is inserted to
+        :type columns: List[str]
+        :param section: currently only supports 'dev',
+         defaults to 'dev'
+        :type section: str, optional
+        """
         try:
             self.connect(section)
             with self.conn.cursor() as curr:
                 # get the column names
                 col_names = ",".join(columns)
+
                 # create an iterator of tuple rows for db insert
                 args = (tuple(item.values()) for item in data)
-                query = """INSERT INTO {} ({}) values %s""".format(table_name, col_names)
+                query = f"""INSERT INTO {table_name} ({col_names}) values %s"""
+
+                # insert data into database and close connection
                 execute_values(curr, query, args)
             self.conn.commit()
             self.conn.close()
         except psycopg2.DatabaseError as e:
             print(e)
         else:
-            return 
+            return
 
-    def pushdf(self, datadf: pd.DataFrame, table_name: str, section: str = 'dev') -> None:
-        """pushes a pandas dataframe to DataBase"""
+    @execution_time
+    def pushdf(self,
+               datadf: pd.DataFrame,
+               table_name: str,
+               section: str = 'dev',
+               ) -> None:
+        """Pushes pandas dataframe to database
+
+        :param datadf: data to be pushed
+        :type datadf: pd.DataFrame
+        :param table_name: the name of table to be pushed to
+        :type table_name: str
+        :param section: currently only supports 'dev',
+        defaults to 'dev'
+        :type section: str, optional
+        """
+        # get the column names
         col_names = list(datadf)
+
+        # create an iterator from pandas df prior to push
         data = self.iterator_from_df(datadf)
-        self.push(data, table_name=table_name, columns=col_names, section=section)
-        return 
+
+        # push the data to table given by table_name
+        self.push(data,
+                  table_name=table_name,
+                  columns=col_names,
+                  section=section)
+        return
 
     def copy_from(self, data: Iterator[Dict[str, Any]], table_name: str, columns: List[str], section: str = 'dev') -> None:
         """copies data from csv file and writes to Db"""
@@ -151,21 +201,7 @@ class DbReader:
         query = f'drop table {table_name};'
         self.execute(query, conn)
 
-    def readTable(self, table_name: str, limit: int=10, section: str='dev'):
-        """Reads contents of a table given by table_name
-        Args:
-        table_name (str): name of the table
-        limit (int):      limit observations in table
-
-        Returns:
-        Table (DataFrame)
-        """
-        query = f"""select * from {table_name}"""
-        if limit:
-            query += f" " + f"""limit {limit}"""
-        return self.fetchdf(query, section=section)
-
-
+    
 
     def delete(self, table_name, section='dev'):
         """deletes all rows given by table_name from dev deb
