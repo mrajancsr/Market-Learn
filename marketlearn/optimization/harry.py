512e58bc4fe9c969c05b656a8434b77d2af29fde
RajanS <mrajancsr@gmail.com>
2021-02-07 01:44:20 -0500

    started work on portfolio optimization, but still have lots fo work to do

--- marketlearn/optimization/harry.py
+++ marketlearn/optimization/harry.py
@@ -1,7 +1,7 @@
 """
 Implementation of Harry Markowitz's Modern Portfolio Theory
-Author: Rajan Subramanian 
-Created May 15 2020
+Author: Rajan Subramanian
+Created Feb 10 2021
 """
 
 import numpy as np
@@ -13,11 +13,11 @@ class Harry:
 
     Parameters
     ----------
-    mu_vec : np.ndarray
+    means : np.ndarray, shape=(p_constituents,)
         represents forecasted means of constituents in portfolio
-    sigma_vec : np.ndarray
+    sigmas : np.ndarray, shape=(p_constituents,)
         represents forecasted volatility of constituents in portfolio
-    cov_mat : np.ndarray
+    cov : np.ndarray, shape=(p_constituents, p_constituents)
         covariance matrix of constituents in portfolio
 
     Attributes
@@ -28,11 +28,45 @@ class Harry:
         total number of securities in portfolio
     """
 
-    def __init__(self, mu_vec: np.ndarray, sigma_vec: np.ndarray, cov_mat: np.ndarray):
-        self.mu_vec = mu_vec
-        self.sigma_vec = sigma_vec
-        self.cov_mat = cov_mat
-        self.size = len(mu_vec)
+    def __init__(self, means, sigmas, cov):
+        self.means = means
+        self.sigmas = sigmas
+        self.cov = cov
+        self.weights = np.zeros_like(means)
+        self.size = len(means)
 
-    def compute_portfolio_mean(self) -> float:
-        pass
+    def get_portfolio_mean(self) -> float:
+        """Computes the portfolio mean
+
+        Returns
+        -------
+        float
+            portfolio mean
+        """
+        return self.weights.T @ self.means
+
+    def get_portfolio_variance(self) -> float:
+        """Computes the portfolio variance
+
+        Returns
+        -------
+        float
+            [description]
+        """
+        return self.weights.T @ self.cov @ self.weights
+
+    def global_minimum_variance(self, mean_constraint=False, target=None) -> np.ndarray:
+        """Computes the weights corresponding to global minimum variance
+
+        Parameters
+        ----------
+        mean_constraint : bool, optional
+            if true, add the constraint w'mu=target
+        target : float, optional, default=None
+            if mean_constraint is True, add target
+
+        Returns
+        -------
+        np.ndarray
+            weights corresponding to minimum variance
+        """
