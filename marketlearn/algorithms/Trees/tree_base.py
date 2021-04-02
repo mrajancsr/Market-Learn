89b76418ade64a996e7cb3050b352583acc8acc4
RajanS <mrajancsr@gmail.com>
2021-04-02 01:09:27 -0400

    cleaned up the tree base class

--- marketlearn/algorithms/Trees/tree_base.py
+++ marketlearn/algorithms/Trees/tree_base.py
@@ -3,29 +3,76 @@ from marketlearn.algorithms.linked_collections import LinkedQueue
 
 class Tree:
     """Abstract Base Class representing tree structure"""
+
     class Position:
         """Abstraction representing location of a single element"""
 
         def element(self):
-            """Returns elements stored in this position"""
-            raise NotImplemented("must be implemented by subclass")
+            """Returns elements stored in this position
+
+            Raises
+            ------
+            NotImplemented
+                must be implemented by subclass
+            """
+            raise NotImplementedError("must be implemented by subclass")
 
         def __eq__(self, other):
-            """returns True if other position represents same location"""
-            raise NotImplemented("must be implemented by subclass")
+            """returns True if other position represents same location
+
+            Parameters
+            ----------
+            other : Position
+                location of element
+
+            Raises
+            ------
+            NotImplemented
+                must be implemented by subclass
+            """
+            raise NotImplementedError("must be implemented by subclass")
 
         def __ne__(self, other):
-            """returns Trueif other does not represent the same location"""
+            """returns True if other does not represent the same location
+
+            Parameters
+            ----------
+            other : Position
+                position of single element
+
+            Returns
+            -------
+            bool
+                return True if other does not represent same location
+                False otherwsie
+            """
             return not (self == other)
 
     # abstract methods that concrete subclass must support--------------------
     def root(self):
-        """return position representing tree's root or None if empty"""
-        raise NotImplemented("must be implemented by subclass")
+        """return position representing tree's root or None if empty
+
+        Raises
+        ------
+        NotImplementedError
+            must be implemented in subclass
+        """
+        raise NotImplementedError("must be implemented by subclass")
 
     def parent(self, p):
-        """return position representing p's parent (or None if p is root)"""
-        raise NotImplemented("must be implemented by subclass")
+        """return position representing p's parent (or None if p is root)
+
+        Parameters
+        ----------
+        p : Position
+            represent location of element
+
+        Raises
+        ------
+        NotImplementedError
+            must be implemented in subclass
+        """
+        raise NotImplementedError("must be inmplemented in subclass")
 
     def num_children(self, p):
         """return # of children that position p has"""
@@ -41,13 +88,13 @@ class Tree:
 
     def is_root(self, p):
         """return true if position p represents root of tree
-            takes O(1) time
+        takes O(1) time
         """
         return p == self.root()
 
     def is_leaf(self, p):
         """return true if position p does not have any children
-            takes O(1) time since num_children takes O(1) time
+        takes O(1) time since num_children takes O(1) time
         """
         return self.num_children(p) == 0
 
@@ -57,13 +104,13 @@ class Tree:
 
     def is_empty(self):
         """Return True if tree is empty
-            takes O(1) time
+        takes O(1) time
         """
         return len(self) == 0
 
     def depth(self, p):
         """returns # of levels seperating position p from root
-            takes O(n) worse time
+        takes O(n) worse time
         """
         if self.is_root(p):
             return 0
@@ -71,16 +118,16 @@ class Tree:
             return 1 + self.depth(self.parent(p))
 
     def _height1(self, p):
-        """ returns height of subtree rooted at position p
-            height of non empty tree T is is max of depth
-            of its leaf positions
-            Takes O(n^2) worse time
+        """returns height of subtree rooted at position p
+        height of non empty tree T is is max of depth
+        of its leaf positions
+        Takes O(n^2) worse time
         """
         return max(self.depth(p) for p in self.positions() if self.is_leaf(p))
 
     def _height2(self, p):
         """returns height of subtre rooted at position p
-            takes O(n) time
+        takes O(n) time
         """
         if self.is_leaf(p):
             return 0
@@ -89,8 +136,8 @@ class Tree:
 
     def height(self, p=None):
         """returns height of subtree rooted at position p
-            params: p (position p, default = None for entire tree height)
-            takes O(n) time
+        params: p (position p, default = None for entire tree height)
+        takes O(n) time
         """
         if p is None:
             p = self.root()
@@ -103,7 +150,7 @@ class Tree:
 
     def breadthfirst(self):
         """performs a breadth first tree traversal on tree
-            Uses a LinkedQueue implementation"""
+        Uses a LinkedQueue implementation"""
         if not self.is_empty():
             lq = LinkedQueue()
             lq.enqueue(self.root())
@@ -130,13 +177,13 @@ class _BinaryTreeBase(Tree):
 
     def right(self, p):
         """return position representing p's right child
-            return None if p does not have right child"""
+        return None if p does not have right child"""
 
         raise NotImplemented("must be implemented by subclass")
 
     def sibling(self, p):
         """return position representing p's sibling (None if no siblings
-            takes O(1) time
+        takes O(1) time
         """
         parent = self.parent(p)
         if parent is None:
