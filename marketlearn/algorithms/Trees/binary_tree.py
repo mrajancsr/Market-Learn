ca16b7191290ee43c93eb9501c7aab9c7184a066
RajanS <mrajancsr@gmail.com>
2021-04-03 19:29:19 -0400

    modified trees further

--- marketlearn/algorithms/Trees/binary_tree.py
+++ marketlearn/algorithms/Trees/binary_tree.py
@@ -5,7 +5,7 @@ Date: 11/11/2020
 """
 from __future__ import annotations
 from marketlearn.algorithms.trees import tree_base as tb
-from typing import Union
+from typing import Any, Union
 
 
 class BinaryTree(tb._BinaryTreeBase):
@@ -40,11 +40,31 @@ class BinaryTree(tb._BinaryTreeBase):
         return self.Position(self, node) if node is not None else None
 
     def _validate(self, p: Position) -> _Node:
-        """return position's node or raise appropriate error if invalid"""
+        """return position's node or raise appropriate error if invalid
+
+        Parameters
+        ----------
+        p : Position
+            represents the position of interest
+
+        Returns
+        -------
+        _Node
+            position's node object
+
+        Raises
+        ------
+        TypeError
+            if p is not a proper Position
+        TypeError
+            if p does not belong to same container
+        ValueError
+            if p's parent is the current node
+        """
         if not isinstance(p, self.Position):
-            raise ("p must be proper Position type")
+            raise TypeError("p must be proper Position type")
         if p._container is not self:
-            raise ValueError("p does not belong to this container")
+            raise TypeError("p does not belong to this container")
 
         # convention for deprecated nodes
         if p._node._parent is p._node:
@@ -58,19 +78,25 @@ class BinaryTree(tb._BinaryTreeBase):
         self._size = 0
 
     def __len__(self):
-        """returns total number of nodes in a tree
-        takes O(1) time"""
+        """returns total number of nodes in a tree"""
         return self._size
 
     def root(self) -> Position:
-        """return root position of tree, return None if tree is empty
-        takes O(1) time
-        """
+        """return root position of tree, return None if tree is empty"""
         return self._make_position(self._root)
 
-    def parent(self, p) -> Union[Position, None]:
+    def parent(self, p: Position) -> Union[Position, None]:
         """return position representing p's parent (or None if p is root)
-        takes O(1) time
+
+        Parameters
+        ----------
+        p : Position
+            position who's parent we are interested in
+
+        Returns
+        -------
+        Union[Position, None]
+            position representing p's parent or None if p is root
         """
         node = self._validate(p)
         return self._make_position(node._parent)
@@ -124,7 +150,7 @@ class BinaryTree(tb._BinaryTreeBase):
             count += 1
         return count
 
-    def _add_root(self, data):
+    def _add_root(self, data: Any):
         """place data at root of empty tree and return new position
         raise ValueError if tree nonempty
         takes O(1) time
@@ -135,7 +161,7 @@ class BinaryTree(tb._BinaryTreeBase):
         self._root = self._Node(data)
         return self._make_position(self._root)
 
-    def _add_left(self, p, data):
+    def _add_left(self, p, data: Any):
         """place data at left child of position p
         raise valueError if left child already exists
         takes O(1) time
@@ -146,7 +172,7 @@ class BinaryTree(tb._BinaryTreeBase):
         node._left = self._Node(data, parent=node)
         return self._make_position(node._left)
 
-    def _add_right(self, p, data):
+    def _add_right(self, p, data: Any):
         """place data at right child of position p
         raise valueError if right child already exists
         takes O(1) time
@@ -157,7 +183,7 @@ class BinaryTree(tb._BinaryTreeBase):
         node._right = self._Node(data, parent=node)
         return self._make_position(node._right)
 
-    def _replace(self, p, data):
+    def _replace(self, p, data: Any):
         """replace data at position p with data and returns old data
         takes O(1) time
         """
