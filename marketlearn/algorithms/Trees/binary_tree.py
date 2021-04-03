64873ecb9e3b91d7ae1c5acca251dbb23bce5085
RajanS <mrajancsr@gmail.com>
2021-04-03 18:12:54 -0400

    cleaned up trees

--- marketlearn/algorithms/Trees/binary_tree.py
+++ marketlearn/algorithms/Trees/binary_tree.py
@@ -3,10 +3,10 @@
 Author: Rajan Subramanian
 Date: 11/11/2020
 """
-from marketlearn.algorithms.trees.tree_base import _BinaryTreeBase
+from marketlearn.algorithms.trees import tree_base as tb
 
 
-class BinaryTree(_BinaryTreeBase):
+class BinaryTree(tb._BinaryTreeBase):
     """Class representing binary tree structure using linked representation
 
     Attributes:
@@ -22,7 +22,7 @@ class BinaryTree(_BinaryTreeBase):
 
     class _Node:
 
-        __slots__ = '_element', '_parent', '_left', '_right'
+        __slots__ = "_element", "_parent", "_left", "_right"
 
         def __init__(self, element, parent=None, left=None, right=None):
             self._element = element
@@ -30,7 +30,7 @@ class BinaryTree(_BinaryTreeBase):
             self._left = left
             self._right = right
 
-    class Position(_BinaryTreeBase.Position):
+    class Position(tb.Position):
         """Abstraction representing location of single element"""
 
         def __init__(self, container, node):
@@ -51,7 +51,7 @@ class BinaryTree(_BinaryTreeBase):
     def _validate(self, p):
         """return position's node or raise appropriate error if invalid"""
         if not isinstance(p, self.Position):
-            raise("p must be proper Position type")
+            raise ("p must be proper Position type")
         if p._container is not self:
             raise ValueError("p does not belong to this container")
         if p._node._parent is p._node:
@@ -61,45 +61,47 @@ class BinaryTree(_BinaryTreeBase):
     # binary tree constructor
     def __init__(self):
         """Creates a initially empty binary tree
-            takes O(1) time
+        takes O(1) time
         """
         self._root = None
         self._size = 0
-        self._my_hash = {'preorder': self.preorder,
-                         'postorder': self.postorder,
-                         'inorder': self.inorder,
-                         "breadthfirst": self.breadthfirst}
+        self._my_hash = {
+            "preorder": self.preorder,
+            "postorder": self.postorder,
+            "inorder": self.inorder,
+            "breadthfirst": self.breadthfirst,
+        }
 
     def __len__(self):
         """returns total number of nodes in a tree
-            takes O(1) time """
+        takes O(1) time"""
         return self._size
 
     def root(self):
         """return root position of tree, return None if tree is empty
-            takes O(1) time
+        takes O(1) time
         """
         return self._make_position(self._root)
 
     def parent(self, p):
         """return position representing p's parent (or None if p is root)
-            takes O(1) time 
+        takes O(1) time
         """
         node = self._validate(p)
         return self._make_position(node._parent)
 
     def left(self, p):
         """return position representing p's left child
-            return None if p does not have left child
-            takes O(1) time
+        return None if p does not have left child
+        takes O(1) time
         """
         node = self._validate(p)
         return self._make_position(node._left)
 
     def right(self, p):
         """return position representing p's right child
-            return None if p does not have right child
-            takes O(1) time
+        return None if p does not have right child
+        takes O(1) time
         """
 
         node = self._validate(p)
@@ -107,7 +109,7 @@ class BinaryTree(_BinaryTreeBase):
 
     def num_children(self, p):
         """return # of children that position p has
-            takes O(1) time
+        takes O(1) time
         """
         node = self._validate(p)
         count = 0
@@ -119,8 +121,8 @@ class BinaryTree(_BinaryTreeBase):
 
     def _add_root(self, data):
         """place data at root of empty tree and return new position
-            raise ValueError if tree nonempty
-            takes O(1) time
+        raise ValueError if tree nonempty
+        takes O(1) time
         """
         if self._root is not None:
             raise ValueError("Root exists")
@@ -130,8 +132,8 @@ class BinaryTree(_BinaryTreeBase):
 
     def _add_left(self, p, data):
         """place data at left child of position p
-            raise valueError if left child already exists
-            takes O(1) time
+        raise valueError if left child already exists
+        takes O(1) time
         """
         node = self._validate(p)
         if node._left is not None:
@@ -141,8 +143,8 @@ class BinaryTree(_BinaryTreeBase):
 
     def _add_right(self, p, data):
         """place data at right child of position p
-            raise valueError if right child already exists
-            takes O(1) time
+        raise valueError if right child already exists
+        takes O(1) time
         """
         node = self._validate(p)
         if node._right is not None:
@@ -152,7 +154,7 @@ class BinaryTree(_BinaryTreeBase):
 
     def _replace(self, p, data):
         """replace data at position p with data and returns old data
-            takes O(1) time
+        takes O(1) time
         """
         node = self._validate(p)
         old = node._element
@@ -161,9 +163,9 @@ class BinaryTree(_BinaryTreeBase):
 
     def _delete(self, p):
         """delete node at position p and replace it with its child, if any
-            return the data stored at position p
-            raise ValueError if p is invalid or p has 2 children
-            takes O(1) time
+        return the data stored at position p
+        raise ValueError if p is invalid or p has 2 children
+        takes O(1) time
         """
         node = self._validate(p)
         if self.num_children(p) == 2:
@@ -185,7 +187,7 @@ class BinaryTree(_BinaryTreeBase):
 
     def _attach(self, p, tree1=None, tree2=None):
         """Attach trees tree1 and tree2 as left and right subtrees fo external p
-            takes O(1) time
+        takes O(1) time
         """
         node = self._validate(p)
         if not self.is_leaf(p):
@@ -213,22 +215,20 @@ class BinaryTree(_BinaryTreeBase):
                 yield p
 
     def _subtree_preorder(self, p):
-        """generate a preorder iteration of positions in a subtree rooted at position p
-            """
+        """generate a preorder iteration of positions in a subtree rooted at position p"""
         yield p  # visit p first before visiting its subtrees
         for c in self.children(p):
             for pos in self._subtree_preorder(c):
                 yield pos
 
-    def positions(self, type='inorder'):
+    def positions(self, traversal="inorder"):
         """generate iteration of trees positions
-            params:
-            type: (str) tree traversal type, one of pre(post,in)order or breadthfirst
-                    default set to inorder
+        params:
+        type: (str) tree traversal type, one of pre(post,in)order or breadthfirst
+                default set to inorder
         """
-        if type not in ('preorder', 'postorder', 'inorder', 'breadthfirst'):
-            raise AttributeError()
-        return self._my_hash[type]()
+
+        return getattr(self, traversal)()
 
     def _subtree_postorder(self, p):
         """generate postorder iteration of positions in a subtree rooted at p"""
