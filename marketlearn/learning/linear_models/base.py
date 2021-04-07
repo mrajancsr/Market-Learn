13edc0b5b32f6644ae1e10dc7ba1448e15845679
RajanS <mrajancsr@gmail.com>
2021-04-06 20:20:27 -0400

    moved base.py file to linearmodels

--- marketlearn/learning/linear_models/base.py
+++ marketlearn/learning/linear_models/base.py
@@ -8,8 +8,10 @@ from operator import itemgetter
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class LinearBase(metaclass=ABCMeta):
     """Abstract Base class representing the Linear Model"""
+
     @abstractmethod
     def fit(self):
         pass
@@ -18,13 +20,18 @@ class LinearBase(metaclass=ABCMeta):
     def predict(self):
         pass
 
-    def make_regression_example(self, n_samples: int=1000, n_features: int=5) -> Dict:
+    def make_regression_example(
+        self, n_samples: int = 1000, n_features: int = 5
+    ) -> Dict:
         features, output, coef = make_regression(
             n_samples=n_samples,
             n_features=n_features,
-            n_informative=n_features, n_targets=1,
-            noise=5, coef=True)
-        return dict(zip(['X', 'y', 'coef'], [features, output, coef]))
+            n_informative=n_features,
+            n_targets=1,
+            noise=5,
+            coef=True,
+        )
+        return dict(zip(["X", "y", "coef"], [features, output, coef]))
 
     def make_constant(self, X: np.ndarray) -> np.ndarray:
         raise DeprecationWarning("function deprecated -- use make_polynomial instead")
@@ -33,7 +40,7 @@ class LinearBase(metaclass=ABCMeta):
             return np.concatenate((ones, X), axis=1)
         return X
 
-    def make_polynomial(self, X: np.ndarray) -> np.ndarray: 
+    def make_polynomial(self, X: np.ndarray) -> np.ndarray:
         degree, bias = self.degree, self.fit_intercept
         pf = PolynomialFeatures(degree=degree, include_bias=bias)
         return pf.fit_transform(X)
@@ -42,8 +49,9 @@ class LinearBase(metaclass=ABCMeta):
         plt.figure(figsize=(10, 6))
         plt.scatter(X, y)
         # sort by design matrix -- needed for matplotlib
-        sorted_values = iter(sorted(zip(X.flatten(), self.predictions),
-                             key=itemgetter(0)))
+        sorted_values = iter(
+            sorted(zip(X.flatten(), self.predictions), key=itemgetter(0))
+        )
         X, pred = zip(*sorted_values)
-        plt.plot(X, pred, 'm-')
+        plt.plot(X, pred, "m-")
         plt.title("Regression Plot")
