373f0a5acbec93572a19445b65f68828443a94f7
RajanS <mrajancsr@gmail.com>
2021-04-13 21:20:59 -0400

    made more design changes to harry

--- marketlearn/optimization/harry.py
+++ marketlearn/optimization/harry.py
@@ -7,6 +7,7 @@ Created Feb 10 2021
 from __future__ import annotations
 from marketlearn.optimization import Asset
 from numpy import fromiter
+from typing import Iterator
 import numpy as np
 import pandas as pd
 
@@ -17,22 +18,23 @@ class Harry:
 
     Parameters
     ----------
-    means : np.ndarray, shape=(p_constituents,)
-        represents forecasted means of constituents in portfolio
-    sigmas : np.ndarray, shape=(p_constituents,)
-        represents forecasted volatility of constituents in portfolio
-    cov : np.ndarray, shape=(p_constituents, p_constituents)
-        covariance matrix of constituents in portfolio
+    historical_prices : pd.DataFrame
+        represents historical daily end of day prices
 
     Attributes
     ----------
-    weights: np.ndarray
-        proportion of capital allocated to security i
-    size: np.ndarray
-        total number of securities in portfolio
+    assets: Dict[name: Asset]
+        dictionary of Asset objects whose keys are asset names
+    covariance_matrix: np.ndarray
+        Scaled Covariance Matrix of daily log returns
+    asset_expected_returns: np.ndarray
+        forecasted sample means of each Asset
+    asset_expected_vol: np.ndarray
+        forecasted sample volatility of each Asset
     """
 
     def __init__(self, historical_prices: pd.DataFrame):
+        """Default constructor used to initialize portfolio"""
         self.assets = {
             name: Asset(name=name, price_history=historical_prices)
             for name in historical_prices.columns
@@ -49,19 +51,51 @@ class Harry:
         yield from self.assets.values()
 
     def get_asset(self, name: str) -> Asset:
+        """return the Asset in portfolio given name
+
+        Parameters
+        ----------
+        name : str
+            name of the Asset
+
+        Returns
+        -------
+        Asset
+            contains information about the asset
+        """
         return self.assets[name]
 
-    def get_assets(self):
+    def get_assets(self) -> Iterator[Asset]:
+        """return iteration of assets in a portfolio
+
+        Yields
+        -------
+        Iterator[Asset]
+           returns all assets in the portfolio
+        """
         yield from self
 
     def get_asset_expected_returns(self):
+        """gets expected return of each asset in portfolio
+
+        Yields
+        -------
+        Iterator[float]
+            an iteration of expected return of each asset in portfolio
+        """
         yield from (asset.expected_returns for asset in self)
 
     def get_asset_expected_volatility(self):
-        vols = np.sqrt(
+        """gets expected volatility of each asset in portfolio
+
+        Yields
+        -------
+        Iterator[float]
+            iteration of expected volatility of each asset scaled by trading days
+        """
+        yield from np.sqrt(
             np.diag(self.covariance_matrix) * Asset.get_annualization_factor()
         )
-        yield from vols
 
     def portfolio_mean(self):
         pass
